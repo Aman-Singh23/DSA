a0aed6a04d3e5a1f76133950c01cb8074f9529e1
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-04-16 23:54:21 +0530

    Recursion

---
+++ Recursion/RecursionCombinationFormula.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int comb(int n, int r)
+{
+    if(r == 0 || n == r)
+        return (1);
+    else
+        return comb(n-1, r-1) + comb(n-1, r);
+}
+
+int main()
+{   cout<<comb(5,3);
+    return 0;
+}
