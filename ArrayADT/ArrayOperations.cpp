5c24450031b294fa4b7e13b7ca2f1823fcedb5d1
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-04-28 22:45:19 +0530

    ArrayADT

--- ArrayADT/ArrayOperations.cpp
+++ ArrayADT/ArrayOperations.cpp
@@ -3,11 +3,45 @@ using namespace std;
 
 class ArrayOperation
 {
-    
+    private:
+    int *arr;
+    int size;
+
+    public:
+    ArrayOperation( int arrSize)
+    {
+        size = arrSize;
+        arr = new int[size];
+    }
+
+    void Display()
+    {
+        for(int i=0; i<size; i++)
+        {
+            arr[i];
+        }
+    }
+
+    ~ArrayOperation()
+    {
+        delete[] arr;
+        arr = nullptr;
+    }
 };
 
 int main()
 {
+    int size;
+    cout<<"Enter Size of an Array\n";
+    cin>>size;
+    ArrayOperation list(size);
+
+    for(int i=0; i<size; i++)
+    {
+        
+    }
+
     return 0;
+
 }
 
