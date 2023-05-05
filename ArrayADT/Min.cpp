beda1e2db9c7af292cf79780d07068127dfa0176
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-05-05 23:23:55 +0530

    ArrayADT

---
+++ ArrayADT/Min.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int size=10, length=7;
+    int min;
+
+    int arr[size] = {2,4,6,2,33,9,1};
+    min = INT_MAX;
+
+    for(int i=0; i<length; i++)
+    {
+        if(max<arr[i])
+            min = arr[i];
+    }
+    cout<<max;
+    
+    return 0;
+}
