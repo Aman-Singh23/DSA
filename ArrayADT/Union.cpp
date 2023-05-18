42227b9706c646378e4f875ba1220d141f3697c1
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-05-18 22:12:44 +0530

    ArrayADT

--- ArrayADT/Union.cpp
+++ ArrayADT/Union.cpp
@@ -4,12 +4,32 @@ using namespace std;
 int main()
 {
 
-    int arr1_size = 5;
-    int arr2_size = 4;
+    int A[5] = {1,3,5,6,9};
+    int B[4] = {2,3,6,8};
+    int C[10];
 
-    int arr1[arr1_size] = {1,3,5,6,9};
-    int arr2[arr2_size] = {2,3,6,8};
-    int arr3[arr1_size+arr2_size];
+    int k=0;
+    for(int i=0; i<5; i++)
+    {
+        C[k++] = A[i];
+    }
+
+    for(int j=0; j<4; j++)
+    {
+        for(int i=0; i<5; i++)
+        {
+            if(B[j] == A[i])
+            {
+                break;
+            }
+        }
+        C[k++] = B[j];
+    }
+
+    for(int i=0; i<10; i++)
+    {
+        cout<<C[i]<<" ";
+    }
 
     
 
