4cb641ac40e52bd590098f38819005f2e024f614
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-05-18 22:58:14 +0530

    ArrayADT

---
+++ ArrayADT/Intersection.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int A[5] = {1,3,5,6,9};
+    int B[4] = {2,3,6,8};
+    int C[10];
+
+    int k=0;
+    for(int i=0; i<5; i++) {
+        for(int j=0; j<4; j++) {
+            if(A[i] == B[j]) {
+                C[k++] = A[i];
+            }
+        }
+    }
+
+    for(int i=0; i<k; i++) {
+        cout<<C[i]<<" ";
+    }
+
+    
+
+    return 0;
+}
