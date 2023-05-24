fe587e825c99232e16d4031f42e7916374a14523
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-05-24 22:31:48 +0530

    ArrayADT

--- ArrayADT/ArrayOperations.cpp
+++ ArrayADT/ArrayOperations.cpp
@@ -3,45 +3,73 @@ using namespace std;
 
 class ArrayOperation
 {
-    private:
-    int *arr;
-    int size;
+private:
+    int *A;
+    int *B;
+    int *C;
+    int length_A;
+    int length_B;
 
-    public:
-    ArrayOperation( int arrSize)
+public:
+    ArrayOperation(int* arr_A, int arr_A_length, int* arr_B, int arr_B_length)
     {
-        size = arrSize;
-        arr = new int[size];
+        length_A = arr_A_length;
+        length_B = arr_B_length;
+
+        A = new int[length_A];
+        for(int i=0; i<length_A; i++) {
+            A[i] = arr_A[i];
+        }
+
+        B = new int[length_B];
+        for(int i=0; i<length_B; i++) {
+            B[i] = arr_B[i];
+        }
+
+        C = new int[0];
     }
 
-    void Display()
+    void display(int* arr, int length)
     {
-        for(int i=0; i<size; i++)
-        {
-            arr[i];
+        for(int i=0; i<length; i++) {
+            cout<<arr[i]<<" ";
         }
+        cout<<endl;
     }
 
     ~ArrayOperation()
     {
-        delete[] arr;
-        arr = nullptr;
+        delete[] A;
+        delete[] B;
+        delete[] C;
     }
 };
 
 int main()
 {
-    int size;
-    cout<<"Enter Size of an Array\n";
-    cin>>size;
-    ArrayOperation list(size);
+    int size = 10;
+    int length = 3;
+    int newElement = 4;
+    int element = 15;
+    int key = 4;
+    int pos = 2;
+    int elem = 9;
+    int length_B = 4;
 
-    for(int i=0; i<size; i++)
-    {
-        
+    int* A = new int[length];
+    
+    for(int i=0; i<length; i++) {
+        A[i] = i+1;
     }
 
-    return 0;
+    int* B = new int[length_B];
+    
+    for(int i=0; i<length_B; i++) {
+        B[i] = i-50;
+    }
 
+    int* C = new int[0];
+
+    ArrayOperation ops(A, length, B, length_B);
+    ops.display(A, length);
 }
-
