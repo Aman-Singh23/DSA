282206de6cb892ef2ecac9949f711244cd638cbb
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-05-25 22:55:56 +0530

    ArrayADT

---
+++ ArrayADT/MissingElement.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void findMissing(int* A, int& length)
+{
+    int sum = 0, s;
+    for(int i=0; i<length; i++) {
+        sum = sum + A[i];
+    }
+    s = (A[length-1]*(A[length-1]+1))/2;
+    cout<<"Missing element is "<<s-sum;
+}
+
+int main()
+{
+    int length = 5;
+    int* A = new int[length]{1,2,4,5,6};
+    findMissing(A, length);
+
+}
