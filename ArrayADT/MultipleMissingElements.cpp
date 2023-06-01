bec7c6c01df0ca6e78ba4db90e854ddba8614fe8
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-06-01 23:09:30 +0530

    ArrayADT

---
+++ ArrayADT/MultipleMissingElements.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void multipleMissingElements(int* A, int& length)
+{
+    int diff = A[0] - 0;
+    for(int i=0; i<length; i++) {
+        if(A[i]-i != diff) {
+            while(diff < A[i]-i) {
+                cout<<"Missing Element: "<<diff+i<<endl;
+                diff++;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int size = 6;
+    int* A = new int[size]{4,5,7,8,11,20};
+    multipleMissingElements(A, size);
+    return 0;
+}
