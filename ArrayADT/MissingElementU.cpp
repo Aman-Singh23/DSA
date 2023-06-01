c259068bd868b9698e78274d222a75dd158cd4e1
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-06-01 22:59:43 +0530

    ArrayADT

---
+++ ArrayADT/MissingElementU.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void missingElement(int* A, int& length)
+{
+    int diff = A[0] - 0;
+    for(int i=0; i<length; i++) {
+        if(A[i]-i != diff) {
+            cout<<"Missing Element: "<<i+diff<<endl;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int length  = 6;
+    int* A = new int[length]{3,4,6,7,8,9};
+    missingElement(A, length);
+    return 0;
+}
