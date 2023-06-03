cf2e914ecbd3ad98dd6abb25fb1c142fb46a576b
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-06-03 13:52:18 +0530

    ArrayADT

---
+++ ArrayADT/TwoSum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void twoSum(int* A, int& size, int target)
+{
+    for(int i=0; i<size-1; i++) {
+        for(int j=i+1; j<size; j++) {
+            if(A[i] + A[j] == target) {
+                cout<<i<<":"<<A[i]<<" "<<j<<":"<<A[j]<<endl;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int size = 10;
+    int target = 10;
+    int* A = new int[size]{6,3,8,10,16,7,5,2,9,14};
+    twoSum(A, size, target);
+    return 0;
+}
