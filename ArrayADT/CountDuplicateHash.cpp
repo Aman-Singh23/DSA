07d8f569c232cc18b4d0cc6ce439fbb023eed268
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-06-03 12:03:10 +0530

    ArrayADT

---
+++ ArrayADT/CountDuplicateHash.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void countDuplicate(int* A, int& size, int* H, int& length)
+{
+    for(int i=0; i<size; i++) {
+        H[A[i]]++;
+    }
+
+    for(int i=0; i<length; i++) {
+        if(H[i] > 1) {
+            cout<<i<<" : "<<H[i]<<endl;
+        }
+    }
+}
+
+int main()
+{
+    int size = 10, length = 20;
+    int* A = new int[size]{3,6,8,8,10,12,15,15,15,20};
+    int* H = new int[length]{0};
+    countDuplicate(A, size, H, length); 
+    return 0;
+}
