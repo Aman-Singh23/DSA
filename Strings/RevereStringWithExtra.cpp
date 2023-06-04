ec13ef4180aaa5528492b41e7825a48b581dc9f7
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-06-04 22:53:53 +0530

    Strings

---
+++ Strings/RevereStringWithExtra.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char A[] = "Aman Singh";
+    char B[10];
+    int i, j;
+    for(i=0; A[i] !='\0'; i++) {}
+    i = i-1;
+
+    for(j=0; i>=0; i--, j++) {
+        B[j] = A[i];
+    }
+    B[j] = '\0';
+    cout<<B;
+
+}
