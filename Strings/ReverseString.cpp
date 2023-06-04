896baa520f2a17cc22b01dae4f887d8f0fc9ab2b
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-06-04 22:59:52 +0530

    Strings

---
+++ Strings/ReverseString.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char A[] = "Aman Singh";
+    int i, j;
+    for(j=0; A[j]!='\0'; j++) {}
+    j = j-1;
+    for(i=0; i<j; i++, j--) {
+        char temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+    cout<<A;
+}
