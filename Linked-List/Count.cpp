ee26791712ebf3913fa75b54f333dcffaf90b8c9
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-06-19 22:21:04 +0530

    Linked-List

---
+++ Linked-List/Count.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node *next;
+}*first = NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    Node *t, *last;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for(i=1; i<n; i++) {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+int count(Node *p)
+{
+    int countElemnent = 0;
+    while(p != NULL) {
+        countElemnent++;
+        p = p->next;
+    }
+    return countElemnent;
+}
+
+int main()
+{
+    Node *temp;
+    int A[] = {3, 5, 7, 10, 25, 8, 32, 2};
+    create(A, 8);
+    cout<<"Element Count: "<<count(first);
+    return 0;
+}
