f535bc822f1d72d0e2bcdbe0fa8d160d89287ff9
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-06-26 22:44:29 +0530

    Linked-List

---
+++ Linked-List/RecursiveReverse.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node *next;
+}*first = NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    Node *t, *last;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for(i=1; i<n; i++) {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(Node *p)
+{
+    while(p != NULL) {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+}
+
+void reverse(Node *p, Node *q)
+{
+    if(p!=NULL) {
+        reverse(p->next, p);
+        p->next = q;
+    }
+    else {
+        first = q;
+    }
+}
+
+int main()
+{
+    Node *temp = NULL;
+    int A[] = {3, 5, 7, 10, 25, 8, 32, 2};
+    create(A, 8);
+    display(first);
+    cout<<endl;
+    reverse(first, temp);
+    display(first);
+    return 0;
+}
