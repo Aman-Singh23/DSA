915c492e0ac155203aad5b6f8f1ec0b1ea6ef290
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-07-20 22:47:44 +0530

    Linked-List

---
+++ Linked-List/CircularDoubleLLDisplay.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        Node *prev;
+        int data;
+        Node *next;
+}*head = NULL;
+
+void create(int A[], int n)
+{
+    Node *t, *last;
+    int i;
+
+    head = new Node;
+    head->data = A[0];
+    head->prev = head->next = NULL;
+    last = head;
+
+    for(i=1; i<n; i++) {
+        t = new Node;
+        t->data = A[i];
+        t->next = last->next;
+        t->prev = last;
+        last->next =t;
+        last = t;
+    }
+    head->prev = last;
+    last->next = head;
+}
+
+void display(Node *p)
+{
+    do {
+        cout<<p->data<<" ";
+        p = p->next;
+    }
+    while(p!=head);
+}
+
+int main()
+{
+    int A[] = {10, 20, 30, 40, 50};
+    create(A, 5);
+
+    display(head);
+
+    return 0;
+}
