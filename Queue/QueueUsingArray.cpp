75076bc39b9dbbee68eddaa763fcb02a177d372e
Aman-Singh23 <86180971+Aman-Singh23@users.noreply.github.com>
2023-08-01 23:01:06 +0530

    Queue

---
+++ Queue/QueueUsingArray.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class Queue
+{
+    private:
+        int size;
+        int front;
+        int Rear;
+        int *Q;
+
+    public:
+        Queue()
+        {
+            size = 10;
+            front = Rear = -1;
+            Q = new int[size];
+        }
+
+        void enqueue(int x)
+        {
+            if(Rear == size - 1) {
+                cout<<"Queue is Full";
+            }
+            else {
+                Rear++;
+                Q[Rear] = x;
+            }
+        }
+
+        int dequeue()
+        {
+            int x = -1;
+
+            if(front == Rear) {
+                cout<<"Queue is Empty";
+            }
+            else {
+                front++;
+                x = Q[front];
+            }
+            return x;
+        }
+
+        void display()
+        {
+            for(int i=front+1 ; i<=Rear; i++) {
+                cout<<Q[i];
+            }
+            cout<<endl;
+        }
+
+};
+
+int main()
+{
+    Queue q(5);
+    q.enqueue(5);
+    q.enqueue(7);
+    q.enqueue(9);
+    q.enqueue(1);
+
+    q.dequeue();
+
+    return 0;
+}
+
